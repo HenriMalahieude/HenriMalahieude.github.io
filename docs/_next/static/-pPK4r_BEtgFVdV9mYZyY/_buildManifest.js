cd6b8197eea4a63e0e02f3a3775aa57573f2a72e
HenriMalahieude <henrimalahieude@gmail.com>
2023-03-30 16:28:16 -0700

    Create the website finally

---
+++ docs/_next/static/-pPK4r_BEtgFVdV9mYZyY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b72f1cc5f6782def.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
