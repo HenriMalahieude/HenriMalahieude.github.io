1627e66771d88626ea0df3e91f3d2d3030fae418
HenriMalahieude <henrimalahieude@gmail.com>
2023-04-01 00:11:41 -0700

    Create Documents

---
+++ docs/_next/static/nAW-1PvtY3rcCrnKbM8j4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c481c3a37cd08c58.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
