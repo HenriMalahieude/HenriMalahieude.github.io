b9e1050d33db7ce86ee37c5a7b84f3897ce9ada3
HenriMalahieude <henrimalahieude@gmail.com>
2023-04-01 20:52:18 -0700

    Home Tab, Publications Tab, Mega Link Zone, Projects Tab Glo-Up

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,20 @@
-import {useState} from 'react';
 import Head from 'next/head';
 
-function CreateProjectBox({link, image, alt, tooltip, kassign}){
+/* Project Container */
+function CreateProjectBox({link, image, alt, tooltip, kassign, inverted, title, githubLink}){
+	let picClass = "projectImage"
+	if (inverted){
+		picClass = "projectinvertImage"
+	}
+
 	return (
 		<div className = "box" key={kassign}>
 			<a href={link} key={kassign}>
-				<img src={image} alt={alt} />  
+				<img className={picClass} src={image} alt={alt}/>
+				{title && <div className="imageTitle">{title}</div>}
+				{githubLink && <a href={githubLink}><img className="githubIcon" src="pictures/github.png" alt="Project GitHub"/></a>}
 			</a>
+			
 			{tooltip && <span className="tooltiptext">{tooltip}</span>}
 		</div>
 	);
@@ -16,7 +24,7 @@ function FillWithWIP(count){
 	let content = [];
 
 	for (let i = 0; i < count; i++){
-		content.push(<CreateProjectBox link = "https://github.com/HenriMalahieude" image = "wip.png" alt = "WIP" key={String(i*10)}/>);
+		content.push(<CreateProjectBox link = "#Projects" image = "pictures/wip.png" alt = "WIP" key={String(i*10)}/>);
 	}
 
 	return content;
@@ -27,36 +35,54 @@ function ProjectGenerate(){
 	<div className = "projectsContainer">
 		<CreateProjectBox kassign={"1"}
 			link = "https://henrimalahieude.github.io/VisualMazeGenerator/"
-			image = "VisualMazeGenerator.png"
-			alt = "Maze Playground"
-			tooltip="(Requires WebGL)"/>
+			githubLink="https://github.com/HenriMalahieude/VisualMazeGenerator"
+			image = "pictures/VisualMazeGenerator.png"
+			alt = "Maze Playground IMG"
+			title = "Maker and Solver Playground"
+			tooltip={<a href="https://get.webgl.org/">(Requires WebGL)</a>}/>
 		<CreateProjectBox kassign={"2"}
 			link = "https://henrimalahieude.github.io/Chess/"
-			image = "ChessGame.png"
-			alt = "Chess Game"
-			tooltip="(Requires WebGL)"/>
+			githubLink="https://github.com/HenriMalahieude/Chess"
+			image = "pictures/ChessGame.png"
+			alt = "Chess IMG"
+			title = "Chess"
+			tooltip={<a href="https://get.webgl.org/">(Requires WebGL)</a>}/>
 		<CreateProjectBox kassign={"3"}
 			link = "https://pkg.go.dev/github.com/HenriMalahieude/GoStructures"
-			image = "bst.png"
+			githubLink="https://github.com/HenriMalahieude/GoStructures"
+			image = "pictures/bst.png"
+			title="Golang Data-Structures Package"
 			alt = "Structures Library for GoLang"/>
 		<CreateProjectBox kassign={"4"}
 			link = "https://henrimalahieude.github.io/GoPerlinNoise/"
-			image = "perlin.png"
-			alt = "Perlin Noise Generator"
+			githubLink="https://github.com/HenriMalahieude/GoPerlinNoise"
+			image = "pictures/perlin.png"
+			alt = "Perlin IMG"
+			title = "Perlin Noise Generator"
 			tooltip = "(Requires WASM)"/>
 		<CreateProjectBox kassign={"5"}
 			link = "https://youtu.be/Z2UKC_wSfbI"
-			image = "radar.png"
-			alt = "Radar Embedded System Project"/>
+			githubLink="https://github.com/HenriMalahieude/RadarModel"
+			image = "pictures/radar.png"
+			title = "Embedded Systems Final"
+			alt = "Radar IMG"/>
 
-		{FillWithWIP(6)}
-
-		<div className = "box" key={"88"}>
-			<a href="https://github.com/HenriMalahieude">
-				<img className="invert" src="github.png" alt="My Github" />  
-			</a>
-		</div>
+		{FillWithWIP(11)}
 	</div>
+	);
+}
+
+/* List Items */
+function ListItem({image, alt, text, link, tooltip, inverted, title}){
+	return (
+		<div className="listItem" key={String(image) + " " + String(alt)}>
+			<CreateProjectBox link={link} image={image} alt={alt} tooltip={tooltip} kassign="1" inverted={inverted}/>
+			<p className="separator"></p>
+			<div className="txtContainer">
+				<header className="header2">{title}</header>
+				<div>{text}</div>
+			</div>
+		</div>
 	);
 }
 
@@ -74,12 +100,15 @@ function NavBar(){
 export default function MainPage(){
 	//const tabs = ["Home", "Projects", "Publications", "Socials"];
 	//const [curTab, setCurTab] = useState();
+
+	let homeText = "Welcome to Henri's Website. Designed originally with pure HTML and CSS, Henri decided that that his long hours of toil against the browser gods were fruitless. He then switched to NextJS, a React Framework. That way he could remove the pain of working with raw html, and instead work with raw html. Wait."
+	let homeText2 = "Henri Malahieude is an experienced Software Engineer with an extensive history of working in the software industry. Skilled in C++ and Software Development, Henri can execute any solution or problem presented to him. Focused on earning a Bachelor's of Science in Computer Science from University of California - Riverside, Henri has plans to then go into Graduate School for a PhD."
 	
 	return (
 		<div>
 			<Head>
 				<title>{"Henri Malahieude"}</title>
-				<link rel="shortcut icon" href='/favicon.ico' />
+				<link rel="icon" href='favicon.ico' />
 			</Head>
 
 			<link rel="stylesheet" href="style.css"/>
@@ -91,11 +120,28 @@ export default function MainPage(){
 				<div className='tabContainer' id="Publications"> <br/> </div>
 				<div className='tabContainer' id="Links"> <br/> </div>
 			</div>
-			{/*The above thing lets us move side to side*/}
+			{/*The above thing lets us move side to side without fucking the scroll*/}
 			<div className="allContainer">
 				<div className='tabContainer'>
 					<div className='justifier'>
 						<header className='header1'>Home</header>
+						<div> <br/> <br/> <br/> </div>
+						<div className="listItem" key={"Bruh"}>
+							<p className="separator"></p>
+							<div className = "box">
+								<a href="#Home">
+									<img className="meImage1" src="pictures/picofme1.jpg" alt="A picture of me when I was young." />
+								</a>
+							</div>
+							<p className="separator"></p>
+							<div className="txtContainer">
+								<div>{homeText}</div>
+								<div> <br/> </div>
+								<div>{homeText2}</div>
+								<div> <br/> <br/> </div>
+								<div>Click through the navbar, see what I've done so far in my life!</div>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div className='tabContainer'>
@@ -107,11 +153,29 @@ export default function MainPage(){
 				<div className='tabContainer'>
 					<div className='justifier'>
 						<header className='header1'>Publications</header>
+						<ListItem image="pictures/wip.png" link="#Publications" alt="WIP1" tooltip="There ain't nothing here sorry." title="Hoping for success" text="This will be populated as soon as I get more stuff to put here......."/>
 					</div>
 				</div>
 				<div className='tabContainer'>
 					<div className='justifier'>
 						<header className='header1'>Mega Link Zone</header>
+						<ListItem image="pictures/github.png" link="https://github.com/HenriMalahieude" alt="My Public Github Account" inverted="True" title="My Github" text="Just click the icon to get sent to my github!"/>
+						<ListItem image="pictures/linkedin.png" link="https://www.linkedin.com/in/henri-malahieude/" alt="My Linkedin Account" title="My Linkedin" text="Just click the icon to get sent to my Linkedin!"/>
+						
+						<div className="listItem" key={"Was that a short joke?"}>
+							<div className = "box">
+								<a href="files/current_resume.pdf" download="Henri Malahieude's Current Resume">
+									<img className="projectImage" src="pictures/resume_thumbnail.png" alt="Resume Thumbnail" />
+								</a>
+								
+								<span className="tooltiptext">Click To Download!</span>
+							</div>
+							<p className="separator"></p>
+							<div className="txtContainer">
+								<header className="header2">Henri's Resume</header>
+								<div>Click on the thumbnail!<br/>Last Updated: April 1st, 2023</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
